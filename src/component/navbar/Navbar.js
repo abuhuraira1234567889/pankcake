736a63442bd78ed5b459eb3cfe7a7383b3bea2bb
abuhuraira1234567889 <hurairaabu098@gmail.com>
2022-03-02 10:46:53 +0500

    init

---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useState,useEffect} from 'react';
+import "./navbar.css";
+
+const Navbar = () => {
+    const [slide,setSlide ] = useState(false);
+    const[screen,setScreen]=useState(window.innerWidth)
+    const navOpen = ()=> {
+        setSlide(!slide)
+    }
+    useEffect(()=>{
+        const changeWidth=()=>{
+            setScreen(window.innerWidth)
+        }
+        window.addEventListener("resize",changeWidth)
+
+    },[])
+    
+
+    return (
+
+        <>
+            <div className="navbar-wrapper">
+                <div className="container">
+                    <div className="nav-main">
+                        <div className="nav-logo">
+                            <h2>Navbar</h2>
+
+                        </div>
+                        { (slide || screen > 767) &&(
+                            <div className="nav-links">
+                                <a href="">Home</a>
+                                <a href="">Win</a>
+                                <a href="">Trade</a>
+                            
+
+                        </div>
+
+                        )
+                        }
+                        
+                            
+
+
+                        
+
+                        
+
+                        <div className="lines" onClick={navOpen} >
+                            <div className="line1">  </div>
+                            <div className="line2"></div>
+                            <div className="line3"></div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+
+export default Navbar;
